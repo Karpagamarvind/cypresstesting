8d810a44fc12247413fe1ed3fea8c7fc40b72ea2
Karpagamarvind <karpagamarvind88@gmail.com>
2021-08-06 00:24:19 -0400

    added first

---
+++ cypressAutomation/cypress/integration/2-advanced-examples/basic/filterreduceandmap.js
@@ -0,0 +1,15 @@
+var array =[12,13,14,15,16]
+
+//var evenarray=array.filter(score=>score%2==0)
+
+//console.log(evenarray)
+
+//var mapparray=evenarray.map(score=>score*3)
+
+//console.log(mapparray)
+
+let final=array.filter(score=>score%2==0).map(score=>score*3).reduce((sum,val)=>sum+val,0)
+
+console.log(final)
+
+hhujojljo
